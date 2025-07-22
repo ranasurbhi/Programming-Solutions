098935f1a52b3caf2062debff5c160d73fde7334
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-07-22 10:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (16.74%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -10,16 +10,18 @@
  * };
  */
 class Solution {
-    void preorder(TreeNode* root, int &count){
-        if(!root) return;
+    void traversal(TreeNode* root, int& count){
+        if(!root){
+            return;
+        }
         count++;
-        preorder(root->left,count);
-        preorder(root->right,count);
+        traversal(root->left,count);
+        traversal(root->right,count);
     }
 public:
     int countNodes(TreeNode* root) {
         int count=0;
-        preorder(root,count);
+        traversal(root,count);
         return count;
     }
 };
