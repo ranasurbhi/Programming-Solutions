e0b600844eb6147103ee285c56d589f6819a06b4
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-08-06 10:16:11 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (88.62%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int freshOrange=0;
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> v(n,vector<int>(m,0));
+      
+        int fresh=0;
         queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==1) freshOrange++;
+                if(grid[i][j]==1){
+                    fresh++;
+                }
                 else if(grid[i][j]==2){
                     q.push({i,j});
-                    v[i][j]=1;
                 }
             }
         }
-
-        if(!freshOrange) return 0;
-        int time=0;
+        if(fresh==0) return 0;
+        int count=0;
         while(!q.empty()){
-            int sz = q.size();
-            for (int t = 0; t < sz; t++) {
+            int s=q.size();
+            for(int k=0;k<s;k++){
                 pair<int,int> temp=q.front();
                 q.pop();
                 int i=temp.first;
                 int j=temp.second;
-                // top i-1 j
-                if(i-1>=0 && v[i-1][j]==0 && grid[i-1][j]==1){
-                    v[i-1][j]=1;
+                // i-1 j
+                if(i-1>=0 && grid[i-1][j]==1){
                     q.push({i-1,j});
-                    freshOrange--;
+                    grid[i-1][j]=2;
+                    fresh--;
                 }
-                // bottom i+1 j
-                if(i+1<n && v[i+1][j]==0 && grid[i+1][j]==1){
-                    v[i+1][j]=1;
-                    q.push({i+1,j});
-                    freshOrange--;
-                }
-                // left i j-1
-                if(j-1>=0 && v[i][j-1]==0 && grid[i][j-1]==1){
-                    v[i][j-1]=1;
-                    q.push({i,j-1});
-                    freshOrange--;
-                }
-                // right i j+1
-                if(j+1<m && v[i][j+1]==0 && grid[i][j+1]==1){
-                    v[i][j+1]=1;
+                // i j+1
+                if(j+1<m && grid[i][j+1]==1){
                     q.push({i,j+1});
-                    freshOrange--;
+                    grid[i][j+1]=2;
+                    fresh--;
+                }
+                // i+1 j
+                if(i+1<n && grid[i+1][j]==1){
+                    q.push({i+1,j});
+                    grid[i+1][j]=2;
+                    fresh--;
+
+                }
+                // i j-1
+                if(j-1>=0 && grid[i][j-1]==1){
+                    q.push({i,j-1});
+                    grid[i][j-1]=2;
+                    fresh--;
                 }
             }
-            time++;
+            count++;
         }
-
-        if(freshOrange) return -1;
-        return time-1;
+        if(fresh!=0) return -1;
+        return count-1;
         
     }
 };
