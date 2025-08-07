636788fc07867debc30bf346e4c33c8927d058c3
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-08-07 10:16:55 +0530

    Time: 22 ms (47.21%) | Memory: 37.4 MB (57.43%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -3,9 +3,9 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
+        queue<pair<int,int>> q;
         vector<vector<int>> vis(n,vector<int>(m,0));
 
-        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
@@ -14,38 +14,35 @@ public:
                 }
             }
         }
-
-        int level=1;
         while(!q.empty()){
-            int s=q.size();
-            for(int d=0;d<s;d++){
-                pair<int,int> temp=q.front();
-                q.pop();
-                int i=temp.first;
-                int j=temp.second;
-                if(i-1>=0 && mat[i-1][j]==1 && vis[i-1][j]==0){
-                    vis[i-1][j]=1;
-                    mat[i-1][j]=level;
-                    
-                    q.push({i-1,j});
-                }
-                if(i+1<n && mat[i+1][j]==1 && vis[i+1][j]==0){
-                    vis[i+1][j]=1;
-                    mat[i+1][j]=level;
-                    q.push({i+1,j});
-                }
-                if(j-1>=0 && mat[i][j-1]==1 && vis[i][j-1]==0){
-                    vis[i][j-1]=1;
-                    mat[i][j-1]=level;
-                    q.push({i,j-1});
-                }
-                if(j+1<m && mat[i][j+1]==1 && vis[i][j+1]==0){
-                    vis[i][j+1]=1;
-                    mat[i][j+1]=level;
-                    q.push({i,j+1});
-                }
+            pair<int,int> temp=q.front();
+            q.pop();
+            int i=temp.first;
+            int j=temp.second;
+            // i-1 j
+            if(i-1>=0 && !vis[i-1][j]){
+                vis[i-1][j]=1;
+                q.push({i-1,j});
+                mat[i-1][j]=1+mat[i][j];
             }
-            level++;
+            // i+1 j
+            if(i+1<n && !vis[i+1][j]){
+                vis[i+1][j]=1;
+                q.push({i+1,j});
+                mat[i+1][j]=1+mat[i][j];
+            }
+            // i j-1
+            if(j-1>=0 && !vis[i][j-1]){
+                vis[i][j-1]=1;
+                q.push({i,j-1});
+                mat[i][j-1]=1+mat[i][j];
+            }
+            // i j+1
+            if(j+1<m && !vis[i][j+1]){
+                vis[i][j+1]=1;
+                q.push({i,j+1});
+                mat[i][j+1]=1+mat[i][j];
+            }
         }
         return mat;
     }
