0ca927260e7a59bb222ba6a7f8e14fa3462ba723
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-08-23 19:06:17 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (70.99%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,46 +1,46 @@
 class Solution {
-    ListNode* reverse(ListNode* root1, ListNode* root2) {
-        ListNode* curr = root1;
-        ListNode* prev = NULL;
-        while (curr != root2) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+    ListNode* reverse(ListNode* root1,ListNode* root2){
+        ListNode* curr=root1;
+        ListNode* prev=nullptr;
+        while(prev!=root2){
+            ListNode* next=curr->next;
+
+            curr->next=prev;
+            prev=curr;
+            curr=next;
         }
         return prev;
     }
 
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || k == 1) return head;
-
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* prevGroupEnd = &dummy;
-        ListNode* root1 = head;
-
-        while (root1 != NULL) {
-            int temp = 0;
-            ListNode* node = root1;
-
-           
-            while (temp < k && node) {
-                temp++;
-                node = node->next;
+        ListNode* dummy=new ListNode(0);
+        ListNode* root1=head;
+        ListNode* root2=head;
+        ListNode* temp=dummy;
+        while(root1 && root2){
+            // cout<<root1->val<<"="<<root2->val<<endl;
+            for(int i=1;i<k;i++){
+                if(root2) root2=root2->next;
             }
 
-            if (temp < k) break; 
+            if(root2){
+                // cout<<root1->val<<"="<<root2->val<<endl;
+                ListNode* n=root2->next;
+                temp->next=reverse(root1,root2);
+                // cout<<temp->next->val<<endl;
+                temp=root1;
+                // cout<<temp->val<<endl;
+                root1=n;
+                root2=root1;
+            }
 
-            ListNode* root2 = node;
-            ListNode* newStart = reverse(root1, root2);  
-
-            prevGroupEnd->next = newStart;  
-            prevGroupEnd = root1;          
-            root1->next = root2;            
-            root1 = root2;                  
         }
-
-        return dummy.next;
+        if(root1){
+            temp->next=root1;
+            root1=root1->next;
+            temp=temp->next;
+        }
+        return dummy->next;
     }
 };
