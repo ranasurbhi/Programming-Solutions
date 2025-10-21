70923fd6fd84c1fab187268dd3b25a252b375a59
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-10-21 15:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (28.25%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,19 +1,27 @@
 class Solution {
-    int helper(vector<vector<int>>& triangle, int x, int y, vector<vector<int>>& dp){
-        if(x==triangle.size()){
-            return 0;
-        }
+    int helper(vector<vector<int>>& tri, int x, int y, int n, vector<vector<int>>& dp){
+        if(x==n-1 ) return tri[x][y];
+        // if(x>=n ) return INT_MAX;
         if(dp[x][y]!=-1) return dp[x][y];
-        // y
-        int noChange=triangle[x][y]+helper(triangle,x+1,y,dp);
-        // y+1
-        int change=triangle[x][y]+helper(triangle,x+1,y+1,dp);
-        return dp[x][y]=min(change,noChange);
+
+        int down=helper(tri, x+1, y,n, dp);
+        int rightDown=helper(tri, x+1,y+1,n, dp);
+        return dp[x][y]=tri[x][y]+min(down,rightDown);
     }
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        return helper(triangle,0,0,dp);
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n=tri.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+
+        for(int i=0;i<n;i++) dp[n-1][i]=tri[n-1][i];
+
+        for(int x=n-2;x>=0;x--){
+            for(int y=x;y>=0;y--){
+                int down=dp[x+1][y];
+                int rightDown=dp[x+1][y+1];
+                dp[x][y]=tri[x][y]+min(down,rightDown);
+            }
+        }
+        return dp[0][0];
     }
 };
