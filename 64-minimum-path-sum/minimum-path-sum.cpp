a4edd37b11022f53103e3bda845543d9fe8fdd0c
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-10-21 13:43:47 +0530

    Time: 2 ms (37.18%) | Memory: 16.3 MB (39.97%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,22 +1,18 @@
 class Solution {
-    int helper(vector<vector<int>>& grid, int x, int y,int n,int m,vector<vector<int>> &dp){
-        if(x==n-1 && y==m-1){
-            return grid[x][y];
-        }
-        if(x>=n || y>=m) return INT_MAX;
-
+    int helper(vector<vector<int>>& grid,int x, int y,vector<vector<int>>&dp){
+        if(x==0 && y==0) return grid[x][y];
+        if(x<0 || y<0 ) return INT_MAX;
         if(dp[x][y]!=-1) return dp[x][y];
-        // right
-        long long right=helper(grid,x,y+1,n,m,dp);
-        // down
-        long long down=helper(grid,x+1,y,n,m,dp);
-        return dp[x][y]=grid[x][y]+min(right,down);
+
+        int up=helper(grid,x-1,y,dp);
+        int left=helper(grid,x,y-1,dp);
+        return dp[x][y]=grid[x][y]+min(up,left);
     }
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>> dp(n,vector<int> (m,-1));
-        return helper(grid,0,0,n,m,dp);
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return helper(grid,n-1,m-1,dp);
     }
 };
