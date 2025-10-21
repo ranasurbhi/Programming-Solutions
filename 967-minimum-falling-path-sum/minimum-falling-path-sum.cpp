12a762076b53c6f732f01d59df153f0e5ea4a4bd
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-10-21 16:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (18.11%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,32 +1,42 @@
 class Solution {
-    int helper(vector<vector<int>>& matrix, int n, int m, int x, int y,vector<vector<int>> &dp){
-        if(x==n) return 0;
-
-        if(dp[x][y]!=INT_MAX){
+    int helper(vector<vector<int>>& matrix, int x, int y,
+               vector<vector<int>>& dp) {
+        if (y < 0 || y >= matrix.size())
+            return INT_MAX;
+        if (x == 0)
+            return matrix[x][y];
+        if (dp[x][y] != -1)
             return dp[x][y];
-        }
-        // r+1 c-1
-        int same=INT_MAX, pdiag=INT_MAX, ndiag=INT_MAX;
-        if(y-1>=0){
-            pdiag=helper(matrix,n,m,x+1,y-1,dp);
-        }
-        // r+1 c
-        same=helper(matrix,n,m,x+1,y,dp);
-        // r+1 c+1
-        if(y+1<m){
-            ndiag=helper(matrix,n,m,x+1,y+1,dp);
-        }
-        return dp[x][y] = matrix[x][y] + min({pdiag, same, ndiag});
+
+        int up = helper(matrix, x - 1, y, dp);
+        int topleft = helper(matrix, x - 1, y - 1, dp);
+        int topright = helper(matrix, x - 1, y + 1, dp);
+        return dp[x][y] = min(topright, min(up, topleft)) + matrix[x][y];
     }
+
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int minele=INT_MAX;
-        vector<vector<int>> dp(n,vector<int> (m,INT_MAX));
-        for(int i=0;i<m;i++){
-            minele=min(minele,helper(matrix,n,m,0,i,dp));
+        int n = matrix.size();
+        int ans = INT_MAX;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 1e8));
+
+        for (int i = 0; i < n; i++)
+            dp[0][i] = matrix[0][i];
+
+        for (int x = 1; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                
+                int up = dp[x - 1][y];
+                int topright = (y > 0) ? dp[x - 1][y - 1] : INT_MAX;
+                int topleft = (y < n - 1) ? dp[x - 1][y + 1] : INT_MAX;
+                dp[x][y] = min(topright, min(up, topleft)) + matrix[x][y];
+            }
         }
-        return minele;
+
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, dp[n - 1][i]);
+        }
+
+        return ans;
     }
 };
