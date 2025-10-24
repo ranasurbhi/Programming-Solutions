c6ace79fd2d9b8b82709643c4d962448a14e2b9b
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-10-24 13:03:46 +0530

    Time: 4 ms (67.36%) | Memory: 19.1 MB (85.75%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,41 +1,36 @@
 class Solution {
-    bool dfs(vector<vector<int>> &adj, set<int> &st, vector<int> &vis,int &i){
-        vis[i]=1;
-        st.insert(i);
-        int s=adj[i].size();
-        bool res=false;
-        for(int j=0;j<s;j++){
-            if(!vis[adj[i][j]]){
-                res=res || dfs(adj,st,vis,adj[i][j]);
-            }
-            else if(st.count(adj[i][j])){
-                return true;
-            }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int> indegree(V,0);
+        for(int i=0;i<edges.size();i++){
+            indegree[edges[i][1]]+=1;
         }
-        // vis[i]=0;
-        st.erase(i);
-        return res;
+        
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<vector<int>> adj(V);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int temp=q.front();
+            q.pop();
+            ans.push_back(temp);
+            for(int i:adj[temp]){
+                indegree[i]-=1;
+                if(indegree[i]==0) q.push(i);
+            }
+            
+        }
+        return ans;
     }
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        // graph 
-        vector<vector<int>> adj(numCourses);
-        for(auto p:prerequisites){
-            adj[p[1]].push_back(p[0]);
-        }
-
-        vector<int> vis(numCourses,0);
-        
-        // cycle detect
-        bool res=false;
-        set<int> st;
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                res=res || dfs(adj,st,vis,i);
-                // vis[i]=1;
-            }
-
-        }
-        return !res;
+    bool canFinish(int V, vector<vector<int>>& edges) {
+        vector<int> ans=topoSort(V,edges);
+        if(ans.size()==V) return true;
+        return false;
     }
 };
