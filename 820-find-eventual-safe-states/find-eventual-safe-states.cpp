17081f77397148924b64bf4c7d303ce04ea7d95c
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-10-24 13:19:19 +0530

    Time: 124 ms (5.04%) | Memory: 99.5 MB (5.41%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,25 +1,47 @@
 class Solution {
-    bool iscycle(vector<vector<int>>& graph, unordered_set<int> &st, int i,vector<int>&dp){
-        if(dp[i]!=-1) return dp[i];
-        st.insert(i);
-        bool res=false;
-        
-        for(int it:graph[i]){
-            if(st.count(it)) return true;
-            res=res || iscycle(graph,st,it,dp);
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<int> indegree(V,0);
+        for(int i=0;i<edges.size();i++){
+            indegree[edges[i][1]]+=1;
         }
-        st.erase(i);
-        return dp[i]=res;
+        
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        vector<vector<int>> adj(V);
+        for(int i=0;i<edges.size();i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int temp=q.front();
+            q.pop();
+            ans.push_back(temp);
+            for(int i:adj[temp]){
+                indegree[i]-=1;
+                if(indegree[i]==0) q.push(i);
+            }
+            
+        }
+        return ans;
     }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> ans;
-        unordered_set<int> st;
-
-        vector<int> dp(graph.size(),-1);
-        for(int i=0;i<graph.size();i++){
-            if(!iscycle(graph,st,i,dp)) ans.push_back(i);
+        int n=graph.size();
+        vector<vector<int>> edges;
+        for(int i=0;i<n;i++){
+            for(int j:graph[i]){
+                // adj[j].push_back(i);
+                edges.push_back({j,i});
+            }
         }
+
+        
+        vector<int> ans=topoSort(n,edges);
+        sort(ans.begin(),ans.end());
         return ans;
+        // return {};
     }
 };
