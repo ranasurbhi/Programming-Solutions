9edf01b68f81607df61b02b05b4012338cbc93e1
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-12-05 21:39:05 +0530

    Time: 67 ms (9.78%) | Memory: 49.8 MB (19.48%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,29 @@
 class Solution {
-    int helper(vector<int>& coins, int amount, int i, vector<vector<int>>& dp) {
-        if (amount == 0) return 0;    
-        if (i < 0) return INT_MAX;   
-
-        if (dp[i][amount] != -1) return dp[i][amount];
-
-        int take = INT_MAX;
-        if (amount >= coins[i]) {
-            int subproblem = helper(coins, amount - coins[i], i, dp);
-            if (subproblem != INT_MAX)
-                take = 1 + subproblem;  
+    int helper(vector<int>& coins, int amount, int i,vector<vector<int>>& dp){
+        if(i>=coins.size()){
+            if(amount==0) return 0;
+            return INT_MAX;
         }
 
-        // Do not take current coin
-        int nottake = helper(coins, amount, i - 1, dp);
+        if(dp[i][amount]!=-1) return dp[i][amount];
 
-        return dp[i][amount] = min(take, nottake);
+        int take=INT_MAX;
+        if(amount>=coins[i]){
+            int ans=helper(coins,amount-coins[i],i,dp);
+
+            if(ans!=INT_MAX) take=1+ans;
+            
+        }
+        int nottake=helper(coins,amount,i+1,dp);
+        return dp[i][amount]=min(take,nottake);
     }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
 
-        int res = helper(coins, amount, n - 1, dp);
-
-        return (res == INT_MAX) ? -1 : res;
+        int res=helper(coins,amount,0,dp);
+        if(res==INT_MAX) return -1;
+        return res;
     }
 };
