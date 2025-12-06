0dc8220083aafdf35cd6e5d87e9bd1ca7b43227e
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2025-12-06 12:10:28 +0530

    Time: 1747 ms (5.01%) | Memory: 55.6 MB (6.17%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,14 +1,19 @@
 class Solution {
+    bool helper(vector<int>& nums,int i,vector<int> &dp, int n){
+        if(i>=n) return false;
+        if(i==n-1) return true;
+
+        if(dp[i]!=-1) return dp[i];
+        bool res=false;     
+        for(int j=1;j<=min(n,nums[i]);j++){
+            res=res || helper(nums,i+j,dp,n);
+        }
+        return dp[i]=res;
+    }
 public:
     bool canJump(vector<int>& nums) {
-        int i=nums.size()-2;
-        int p=nums.size()-1;
-        while(i>=0){
-            if(p-i<=nums[i]){
-                p=i;
-            }
-            i--;
-        }
-        return p==0;
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return helper(nums,0,dp,n);
     }
 };
