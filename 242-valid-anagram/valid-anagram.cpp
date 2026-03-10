d9ec4ace0038691b28a3995a9b308296e0d9c9f6
ranasurbhi <167668795+ranasurbhi@users.noreply.github.com>
2026-03-10 18:27:24 +0530

    Time: 7 ms (19.50%) | Memory: 9.7 MB (47.04%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_multiset<char> s1(s.begin(),s.end());
-        unordered_multiset<char> t1(t.begin(),t.end());
-        return s1==t1;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
